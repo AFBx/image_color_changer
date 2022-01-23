858024dda1ecc825fa30bef3e5af7b5a2dc7897c
AFBx <ahmeterkin965@gmail.com>
2022-01-23 23:23:45 +0300

    Create Form2.cs

---
+++ Form2.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Demo
+{
+    public partial class Form2 : Form
+    {
+        public Form2()
+        {
+            InitializeComponent();
+        }
+
+        private void Form2_Load(object sender, EventArgs e)
+        {
+
+        }
+        private void ColorPictur()
+        {
+            pictureBox2.Image = ToColorTone(pictureBox1.Image, SelectedColor.BackColor);
+        }
+        public Bitmap ToColorTone(Image image, Color color)
+        {
+            float BrightnessSelection = 255;
+            float scale = BrightnessSelection / 128f;
+
+            float r = color.R / 255f * scale;
+            float g = color.G / 255f * scale;
+            float b = color.B / 255f * scale;
+
+            ColorMatrix cm = new ColorMatrix(new float[][]
+            {
+                new float[] {r, 0, 0, 0, 0},
+                new float[] {0, g, 0, 0, 0},
+                new float[] {0, 0, b, 0, 0},
+                new float[] {0, 0, 0, 1, 0},
+                new float[] {0, 0, 0, 0, 1}
+            });
+            ImageAttributes ImAttribute = new ImageAttributes();
+            ImAttribute.SetColorMatrix(cm);
+
+            Point[] points =
+            {
+                new Point(0, 0),
+                new Point(image.Width - 1, 0),
+                new Point(0, image.Height - 1),
+            };
+            Rectangle rect = new Rectangle(0, 0, image.Width, image.Height);
+
+            Bitmap myBitmap = new Bitmap(image.Width, image.Height);
+            using (Graphics graphics = Graphics.FromImage(myBitmap))
+            {
+                graphics.DrawImage(image, points, rect, GraphicsUnit.Pixel, ImAttribute);
+            }
+            return myBitmap;
+        }
+
+        private void colorSliderVertical1_ColorChanged(object sender, zRack.ColorChangedEventArgs args)
+        {
+            SelectedColor.BackColor = Color.FromArgb(colorSliderVertical1.ColorRGB.R, colorSliderVertical1.ColorRGB.G, colorSliderVertical1.ColorRGB.B);
+            ColorPictur();
+        }
+    }
+}
